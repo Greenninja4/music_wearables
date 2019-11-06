a223b6a30dd96d562b65dafffff8f3f3cc00ae68
Greenninja4 <johnmborn4@gmail.com>
2019-11-05 19:36:12 -0600

    Updated main ino file

--- ECE445/ESPsend_IMU_OSCMsg/ESPsend_IMU_OSCMsg.ino
+++ ECE445/ESPsend_IMU_OSCMsg/ESPsend_IMU_OSCMsg.ino
@@ -1,23 +1,14 @@
-/*---------------------------------------------------------------------------------------------
-  Open Sound Control (OSC) library for the ESP8266/ESP32
-  Example for sending messages from the ESP8266/ESP32 to a remote computer
-  The example is sending "hello, osc." to the address "/test".
-  This example code is in the public domain.
-  --------------------------------------------------------------------------------------------- */
-#if defined(ESP8266)
-#include <ESP8266WiFi.h>
-#else
 #include <WiFi.h>
-#endif
 #include <WiFiUdp.h>
 #include <math.h>
 #include "OSCMessage.h"
 #include "MPU9250.h"
 
-//#include "MahonyAHRS.h"
 extern "C" {
-#include "MahonyAHRS.h"
+//#include "MahonyAHRS.h"
+#include "MadgwickAHRS.h"
 };
+
 // an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
 MPU9250 IMU(Wire, 0x68);
 int status;
@@ -26,9 +17,27 @@ char ssid[] = "OSC_2";          // your network SSID (name)
 char pass[] = "SuperCollider";                    // your network password
 
 WiFiUDP Udp;                                // A UDP instance to let us send and receive packets over UDP
-const IPAddress outIp(192, 168, 1, 126);     // remote IP of your computer
+//const IPAddress outIp(172,20,10,3);     // remote IP of your computer
+const IPAddress outIp(192,168,1,123);     // remote IP of your computer
+//const IPAddress outIp(10,192,202,233);     // remote IP of your computer
 const unsigned int outPort = 57120;          // remote port to receive OSC
+//const unsigned int outPort = 5005;          // remote port to receive OSC
 const unsigned int localPort = 8888;        // local port to listen for OSC packets (actually not used for sending)
+
+float q0Prev = 1.0f, q1Prev = 0.0f, q2Prev = 0.0f, q3Prev = 0.0f;
+OSCMessage msg("/testAddr");
+
+// the number of the LED pin
+const int ledPinR = 16;  // 16 corresponds to GPIO16
+const int ledPinG = 17;  // 16 corresponds to GPIO16
+const int ledPinB = 18;  // 16 corresponds to GPIO16
+
+// setting PWM properties
+const int freq = 5000;
+const int ledChannelR = 2;
+const int ledChannelG = 0;
+const int ledChannelB = 1;
+const int resolution = 8;
 
 void setup() {
   Serial.begin(115200);
@@ -40,15 +49,19 @@ void setup() {
   Serial.println(ssid);
   WiFi.begin(ssid, pass);
 
-  //    while (WiFi.status() != WL_CONNECTED) {
-  //        delay(500);
-  //        Serial.print(".");
-  //    }
-  //    Serial.println("");
+      while (WiFi.status() != WL_CONNECTED) {
+          delay(500);
+          Serial.print(".");
+      }
+      Serial.println("");
 
+  
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+  Serial.println("Subnet mask: ");
+  Serial.println(WiFi.subnetMask());
+  
 
   Serial.println("Starting UDP");
   Udp.begin(localPort);
@@ -58,10 +71,6 @@ void setup() {
 #else
   Serial.println(Udp.localPort());
 #endif
-
-  // serial to display data
-  Serial.begin(115200);
-  while (!Serial) {}
 
   // start communication with IMU
   status = IMU.begin();
@@ -74,59 +83,56 @@ void setup() {
     while (1) {}
   }
 
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannelR, freq, resolution);
+  ledcSetup(ledChannelG, freq, resolution);
+  ledcSetup(ledChannelB, freq, resolution);
+//  
+  // attach the channel to the GPIO to be controlled
+  ledcAttachPin(ledPinR, ledChannelR);
+  ledcAttachPin(ledPinG, ledChannelG);
+  ledcAttachPin(ledPinB, ledChannelB);
+
 }
 
 void loop() {
   // read the sensor
   IMU.readSensor();
-  //   display the data
-//  Serial.print(IMU.getAccelX_mss(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getAccelY_mss(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getAccelZ_mss(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getGyroX_rads(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getGyroY_rads(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getGyroZ_rads(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getMagX_uT(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getMagY_uT(), 6);
-//  Serial.print("\t");
-//  Serial.print(IMU.getMagZ_uT(), 6);
-//  Serial.print("\t");
-//  Serial.println(IMU.getTemperature_C(), 6);
-  MahonyAHRSupdate(IMU.getGyroX_rads(), IMU.getGyroY_rads(), IMU.getGyroZ_rads(), IMU.getAccelX_mss(), IMU.getAccelY_mss(), IMU.getAccelZ_mss(), IMU.getMagX_uT(), IMU.getMagY_uT(), IMU.getMagZ_uT());
-//  Serial.println(q0, 6);
-//  Serial.print("\t");
-//  Serial.println(q1, 6);
-//  Serial.print("\t");
-//  Serial.println(q2, 6);
-//  Serial.print("\t");
-//  Serial.println(q3, 6);
-  //  delay(100);
+
+//  MahonyAHRSupdate(IMU.getGyroX_rads(), IMU.getGyroY_rads(), IMU.getGyroZ_rads(), IMU.getAccelX_mss(), IMU.getAccelY_mss(), IMU.getAccelZ_mss(), IMU.getMagX_uT(), IMU.getMagY_uT(), IMU.getMagZ_uT());
+  MadgwickAHRSupdate(IMU.getGyroX_rads(), IMU.getGyroY_rads(), IMU.getGyroZ_rads(), IMU.getAccelX_mss(), IMU.getAccelY_mss(), IMU.getAccelZ_mss(), IMU.getMagX_uT(), IMU.getMagY_uT(), IMU.getMagZ_uT());
 
   float yaw;
   float pitch;
   float roll;
   
-  yaw   = atan2(2*q1*q2-2*q0*q3,2*q0*q0+2*q1*q1-1)*180/PI;
-  pitch = -1*asin(2*q1*q3+2*q0*q2)*180/PI;
-  roll  = atan2(2*q2*q3-2*q0*q1,2*q0*q0+2*q3*q3-1)*180/PI;
-if(counter%100 < 1){
-  Serial.println(yaw, 6);
-  Serial.println(pitch, 6);
-  Serial.println(roll, 6);
-  Serial.println("_______");
+  yaw   = abs(atan2(2*q1*q2-2*q0*q3,2*q0*q0+2*q1*q1-1)*180/PI);
+  pitch = abs(-1*asin(2*q1*q3+2*q0*q2)*180/PI);
+  roll  = abs(atan2(2*q2*q3-2*q0*q1,2*q0*q0+2*q3*q3-1)*180/PI);
+  if(counter%100 < 1){
+    q0Prev = q0;
+    q1Prev = q1;
+    q2Prev = q2;
+    q3Prev = q3;
+    
+    Serial.println(yaw);
+    Serial.println(pitch);
+    Serial.println(roll);
+    Serial.println("_____________");
+    
 }
 counter++;
-  OSCMessage msg("/testAddr");
+
+  if (counter%10 < 1){
   msg.add(roll).add(pitch).add(yaw);
   Udp.beginPacket(outIp, outPort);
   msg.send(Udp);
   Udp.endPacket();
   msg.empty();
+  }
+
+  ledcWrite(ledChannelR, (int)((yaw/180.0)*255));
+  ledcWrite(ledChannelG, (int)((pitch/90.0)*255));
+  ledcWrite(ledChannelB, (int)((roll/180.0)*255));
+  
 }
